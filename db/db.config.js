238a82320346c2bdf248f0f7374f1ef44f89d79c
jay-sambhu <dellizulter@gmail.com>
2026-01-23 09:25:18 +0545

    data base setup is completed

---
+++ db/db.config.js
@@ -0,0 +1,27 @@
+    const {Sequelize} = require ('sequelize');
+
+    const DATABASE_URL="postgresql://postgres.doftjyooqdlbwzlxboxh:vastu@digita@aws-1-ap-south-1.pooler.supabase.com:6543/postgres?pgbouncer=true"
+
+    const sequelize = new Sequelize(DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    });
+    const connectDb = async () => {
+        try {
+            await sequelize.authenticate();
+
+            await sequelize.sync({alter:false, force:false});
+            
+            console.log('Connection has been established successfully.');
+        } catch (error) {
+            console.error('Unable to connect to the database:', error);
+        }
+    };
+
+    module.exports = {connectDb, sequelize};
