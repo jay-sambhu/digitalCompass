b8fb67a5e0ff54a0d95026a4c1e15c28c1fd7418
jay-sambhu <dellizulter@gmail.com>
2026-02-01 13:22:59 +0545

    updated

--- server/models/index.js
+++ server/models/index.js
@@ -4,15 +4,11 @@ const {DataTypes} = require("sequelize");
 const db = {};
 
 db.userProfile = require("./userModule")(sequelize, DataTypes);
-<<<<<<< HEAD:models/index.js
-db.HouseProject = require("./houseProjectModule")(sequelize,DataTypes);
-db.FloorPlan = require("./floorPlanModule")(sequelize,DataTypes);
-=======
+
 db.houseProject = require("./houseProjectModule")(sequelize,DataTypes);
 db.floorPlan = require("./floorPlanModule")(sequelize,DataTypes);
 db.commercialBuilding = require("./commercialBuildingModule")(sequelize,DataTypes);
 db.commercialFloor = require("./commercialFloorModule")(sequelize,DataTypes);
->>>>>>> bd21c7b4e811fa667f4c1049a546f1882cc9dcc6:server/models/index.js
 
 db.sequelize = sequelize;
 db.DataTypes = DataTypes;
