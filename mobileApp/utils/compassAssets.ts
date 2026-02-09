f5d5f558f245f5da01e083b597ff2aa2aad0560b
jay-sambhu <dellizulter@gmail.com>
2026-02-09 14:32:48 +0545

    dial ui is fixed

--- mobileApp/utils/compassAssets.ts
+++ mobileApp/utils/compassAssets.ts
@@ -18,21 +18,21 @@ const COMPASS_ASSETS = {
     needle: require("../assets/16ZoneVastuCompass/needle.png"),
     icon: require("../assets/normalCompass/icon.png"),
     dialScale: 1.08,
-    dialOffset: { x: 1, y: 0,  },
+    dialOffset: {  x: 5, y: 4 },
   },
   zone32: {
     dial: require("../assets/32ZoneVastuCompass/dial.png"),
     needle: require("../assets/normalCompass/needle.png"),
     icon: require("../assets/normalCompass/icon.png"),
     dialScale: 1.08,
-    dialOffset: { x: 0, y: 0,  },
+    dialOffset: { x: 5, y: 4,  },
   },
   chakra: {
     dial: require("../assets/appliedVastuChakra/dial.png"),
     needle: require("../assets/normalCompass/needle.png"),
     icon: require("../assets/normalCompass/icon.png"),
     dialScale: 1.08,
-    dialOffset: { x: 0, y: 0,  },
+    dialOffset: { x: 5, y: 4,  },
   },
 } as const satisfies Record<CompassType, DialTuning & { dial: any; needle: any; icon: any }>;
 
